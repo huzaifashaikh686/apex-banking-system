45b0a7e1ea30bd33b9006ce573228f6731af3c82
huzaifashaikh686 <huzaifashaikh686@gmail.com>
2026-06-30 16:24:45 +0500

    Refactor Transaction class to use JPA annotations

--- src/main/java/com/example/demo/model/Transaction.java
+++ src/main/java/com/example/demo/model/Transaction.java
@@ -1,44 +1,62 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Transaction {
 
-    private LocalDateTime timestamp;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String tType;
     private double amount;
     private int fromAcc;
     private int toAcc;
 
-    public Transaction(String tType, double amount, int fromAcc, int toAcc){
+    private LocalDateTime timestamp;
+
+    public Transaction() {}
+
+    public Transaction(String tType, double amount, int fromAcc, int toAcc) {
         this.tType = tType;
         this.amount = amount;
         this.fromAcc = fromAcc;
         this.toAcc = toAcc;
+    }
+
+    @PrePersist
+    public void onCreate() {
         this.timestamp = LocalDateTime.now();
     }
 
-    public String getType(){
+    public Long getId() {
+        return id;
+    }
+
+    public String getType() {
         return tType;
     }
 
-    public double getAmount(){
+    public double getAmount() {
         return amount;
     }
 
-    public int getFromAcc(){
+    public int getFromAcc() {
         return fromAcc;
     }
 
-    public int getToAcc(){
+    public int getToAcc() {
         return toAcc;
     }
+
     public LocalDateTime getTimestamp() {
         return timestamp;
     }
 
     @Override
-public String toString() {
-    return "[" + timestamp + "] " + tType + " of " + amount + " from Account #" + fromAcc + " to Account #" + toAcc;
-}
+    public String toString() {
+        return "[" + timestamp + "] " + tType + " of " + amount + " from Account #" + fromAcc + " to Account #" + toAcc;
+    }
 }
