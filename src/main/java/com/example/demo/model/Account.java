633418361b73b528df823ba4651a8800a51d9559
huzaifashaikh686 <huzaifashaikh686@gmail.com>
2026-07-01 11:20:39 +0500

    Refactor Account class with JPA annotations

--- src/main/java/com/example/demo/model/Account.java
+++ src/main/java/com/example/demo/model/Account.java
@@ -1,55 +1,72 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String accHolder;
-    private int accNo;
+
+    @Column(nullable = false)
     private double balance;
 
-    public Account() {}
-    
-    public Account(String accHolder){
+    public Account() {
+    }
+
+    public Account(String accHolder) {
         this.accHolder = accHolder;
-        this.accNo = counter.getAndIncrement();
         this.balance = 0;
     }
 
-    public String getAccHolder(){
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccHolder() {
         return accHolder;
     }
 
-    public int getAccNo(){
-        return accNo;
+    public void setAccHolder(String accHolder) {
+        this.accHolder = accHolder;
     }
 
-    public double getBalance(){
+    public double getBalance() {
         return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public double getAvailableBalance() {
         return balance;
     }
-    
-    protected void credit(double amount){
+
+    protected void credit(double amount) {
         balance += amount;
     }
 
-    protected void debit(double amount){
+    protected void debit(double amount) {
         balance -= amount;
     }
-    
+
     public abstract void deposit(double amount);
+
     public abstract void withdraw(double amount);
+
     public abstract String getAccountType();
-    
-    // Abstract contract to allow dynamic execution in service layers safely
+
     public abstract void applyInterest();
 
     @Override
-    public String toString(){
-        return getAccountType() + " Account | Holder: " + accHolder + " | AccNo: " + accNo + " | Balance: " + balance;
+    public String toString() {
+        return getAccountType() + " Account | Holder: " + accHolder + " | Id: " + id + " | Balance: "
+                + balance;
     }
 }
