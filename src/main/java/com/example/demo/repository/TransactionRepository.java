92f7382cbb0fcc8ee167ce7c357bbedbf2931bfd
huzaifashaikh686 <huzaifashaikh686@gmail.com>
2026-07-01 11:34:49 +0500

    Update TransactionRepository.java

--- src/main/java/com/example/demo/repository/TransactionRepository.java
+++ src/main/java/com/example/demo/repository/TransactionRepository.java
@@ -3,5 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByFromAccOrToAcc(Long fromAcc, Long toAcc);
+
 }
